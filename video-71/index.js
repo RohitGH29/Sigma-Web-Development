cb1a927d4f78d8d64350c48e11bd6843003d3f94
RohitGH29 <rohit055428@gmail.com>
2024-12-31 18:47:56 +0530

    add

---
+++ video-71/index.js
@@ -0,0 +1,29 @@
+// Inserting and Removing Elements using JavaScript DOM Concepts 
+ 
+let element = document.querySelector('.container')
+
+// innerHTML example:
+console.log("innerHTML: ",element.innerHTML = "s" ) 
+
+// innerText example: 
+console.log("innerText: ",element.innerText = "Hello World")
+
+// outerHTML example:
+console.log("outerHTML: ",element.outerHTML )
+
+// outer example:
+console.log("outer: ",element.outer)
+
+// tagName example:
+console.log("tagName" , element.tagName)
+
+// nodeName example:
+console.log("nodeName: ",element.nodeName)
+
+// textContent example:
+console.log("textContent:" ,element.textContent)
+
+// hidden example:
+console.log("hidden: ",element.hidden = true)
+
+// document.designMode = 'on' example:
