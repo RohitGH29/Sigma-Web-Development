09cafdcf29425ab7b425f33262819ddba71c8983
RohitGH29 <rohit055428@gmail.com>
2024-12-31 18:09:34 +0530

    add

---
+++ video-54/script.js
@@ -0,0 +1,16 @@
+alert("Hello");
+
+console.log("Hey I am console log")
+
+var a= prompt("Enter a number")
+var isTrue= confirm("Are you sure to exit this page?")
+
+if(isTrue){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+console.log(a)
+
